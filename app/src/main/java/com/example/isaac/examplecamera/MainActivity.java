213d11ad0ae11f7cc42ebae1c9642723ef784e7f
IsaacSec <isaacsecundino3@gmail.com>
2017-10-05 00:56:59 +0000

    Select photo completed

--- app/src/main/java/com/example/isaac/examplecamera/MainActivity.java
+++ app/src/main/java/com/example/isaac/examplecamera/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.isaac.examplecamera;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -9,7 +13,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     private Button takePicture;
     private Button selectPicture;
     private ImageView picture;
+    private TextView urlView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         takePicture = (Button) findViewById(R.id.b_take);
         selectPicture = (Button) findViewById(R.id.b_select);
         picture = (ImageView) findViewById(R.id.picture);
+        urlView = (TextView) findViewById(R.id.urlpath);
 
         selectPicture.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,6 +79,48 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startIntentSelectPhotos(){
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_EXTERNAL_STORAGE: {
+                // Si la petición se cancela se regresa un arreglo vacío
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permiso concedido
+                    startIntentSelectPhotos();
+                } else {
+                    // Permiso negado
+                    Toast toast = Toast.makeText(this, "Necesitas aceptar los permisos para continuar", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch(requestCode) {
+            case SELECT_PHOTO:
+                if(resultCode == RESULT_OK){
+                    Uri imagenSeleccionada = imageReturnedIntent.getData();
+                    try{
+                        InputStream imagenStream = getContentResolver().openInputStream(imagenSeleccionada);
+                        Bitmap imagen = BitmapFactory.decodeStream(imagenStream);
+                        picture.setImageBitmap(imagen);
+                        urlView.setText(imagenSeleccionada.toString());
+                    }catch (FileNotFoundException fnte){
+                        Toast.makeText(this, fnte.getMessage().toString(), Toast.LENGTH_LONG).show();
+                    }
+                    return;
+                }
+        }
     }
 }
